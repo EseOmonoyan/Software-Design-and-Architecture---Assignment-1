13ac45eda47e57e491bdb6d2a47f5e9002f85221
EseOmonoyan <90115712+EseOmonoyan@users.noreply.github.com>
2025-09-22 21:24:05 -0400

    spelling error fix

--- devices/BrandBBulb.java
+++ devices/BrandBBulb.java
@@ -2,16 +2,16 @@ package devices;
 
 import usage.FileUsageProvider;
 import usage.UsageProvider;
+
 import java.nio.file.Path;
 
-
-public class BrandBBulb extends BaseSmartDevice implements SmartBulb{
-    
+public class BrandBBulb extends BaseSmartDevice implements SmartBulb {
     private final Path dataRoot;
-    public BroadBBulb(String brand, Path dataRoot) {
+    public BrandBBulb(String brand, Path dataRoot) {
         super(brand, "Bulb");
         this.dataRoot = dataRoot;
     }
+    @Override
     protected UsageProvider makeUsageProvider() {
         return new FileUsageProvider(dataRoot.resolve("bulb.txt"));
     }
