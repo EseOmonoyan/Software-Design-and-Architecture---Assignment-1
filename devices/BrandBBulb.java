d6e6e095830115d1796be70c5794095d43ede137
AdityaRamjas <aditya.ramjas@ontariotechu.net>
2025-09-22 20:12:02 -0400

    WIP BrandBulb

--- devices/BrandBBulb.java
+++ devices/BrandBBulb.java
@@ -1,5 +1,18 @@
 package devices;
 
-public class BrandBBulb {
+import usage.FileUsageProvider;
+import usage.UsageProvider;
+import java.nio.file.Path;
+
+
+public class BrandBBulb extends BaseSmartDevice implements SmartBulb{
+    super(brand, "Bulb");
     
+    private final Path dataRoot;
+    public BroadBBulb(String brand, Path dataRoot) {
+        this.dataRoot = dataRoot;
+    }
+    protected UsageProvider makeUsageProvider() {
+        return new FileUsageProvider(dataRoot.resolve("bulb.txt"));
+    }
 }
